5fda296178b2b32db0224452bf70b840fb2d90d8
ADM21JF008009 <92528233+ADM21JF-008009@users.noreply.github.com>
2021-12-17 10:42:48 +0530

    Update OrmLearnApplication.java

--- Stage 3/Spring-data-jpa/Day 1-Session 1 & 2/Handson 2-9/h2/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
+++ Stage 3/Spring-data-jpa/Day 1-Session 1 & 2/Handson 2-9/h2/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -1,89 +1,120 @@
 package com.cognizant.ormlearn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+
 import java.util.List;
 
 import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-
-import com.cognizant.orm_learn.model.Country;
-import com.cognizant.orm_learn.service.CountryService;
-import com.cognizant.spring_learn.service.exception.CountryNotFoundException;
-
 @SpringBootApplication
-//@ComponentScan({"com.*"})
 public class OrmLearnApplication {
-	
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
-	
-	private static CountryService countryService;
-	private static void testGetAllCountries() {
+	public static CountryService countryService;
 
-		LOGGER.info("Start");
+	public static void main(String[] args) throws CountryNotFoundException {
 
-		List<Country> countries =countryService.getAllCountries();
-
-		LOGGER.debug("countries={}", countries);
-
-		LOGGER.info("End");
-
-		}
-	
-	private static void getAllCountriesTest() throws CountryNotFoundException  {
-		LOGGER.info("Start");
-		Country country = countryService.findCountryByCode("IN");
-		 LOGGER.debug("Country:{}", country);
-		LOGGER.info("End");
-		}
-	
-	private static void testAddCountry() throws CountryNotFoundException {
-		Country c=new Country();
-		c.setCode("TC");
-		c.setName("Test Country");
-	
-		countryService.addCountry(c);
+		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+		countryService = context.getBean(CountryService.class);
 		
+		 testGetAllCountries();
+		 testFindCountryByCode();
+		 testAddCountry();
+		 testUpdateCountry();
+		 testDeleteCountry();
+		 
+		 testFindByNameContaining();
+		 testFindByNameContainingOrderByAsc();
+		 testfindByNameStartingWith();
+		 LOGGER.info("Inside main");
+	}
+
+	private static void testGetAllCountries() throws CountryNotFoundException {
+
 		LOGGER.info("Start");
-		Country country = countryService.findCountryByCode("TC");
-		 LOGGER.debug("Country:{}", country);
+		Country country;
+		country = countryService.findCountryByCode("IN");
+		LOGGER.debug("Country:{}", country);
 		LOGGER.info("End");
+
+	}
+
+	private static void testFindCountryByCode() throws CountryNotFoundException {
+		
+			Country country = countryService.findCountryByCode("GB");
+			System.out.println(country.getName());
 		
 	}
+
+	public static void testAddCountry() throws CountryNotFoundException {
+		Country newCountry = new Country();
+		newCountry.setCode("NM");
+		newCountry.setName("Nik");
+		countryService.addCountry(newCountry);
+		System.out.println("Coūntry Added");
+		testFindCountryByCode();
+
+	}
+
 	private static void testUpdateCountry() throws CountryNotFoundException {
-		
-		LOGGER.info("Start");
-		countryService.updateCountry("TC"," I N D I A");
-		 LOGGER.debug("Update Successfull");
-		LOGGER.info("End");
-		
+		countryService.updateCountry("KM", "Megh");
 	}
-	
+
 	private static void testDeleteCountry() {
-		LOGGER.info("Start");
-		countryService.deleteCountry("TG");
-		 LOGGER.debug("TG Delete Successfull");
-		LOGGER.info("End");
+		countryService.deleteCountry("TD");
+		System.out.println("Country Deleted- TD");
 	}
-	
-	
 
+	private static void testFindByNameContaining() {
 
-	public static void main(String[] args) {
+		List<Country> cList = countryService.findByNameContaining("ou");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("COUTRIES CONTAINING ou IN NAME");
+		for (Country c : cList) {
+			System.out.println(c.getCode() + " " + c.getName());
+		}
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+	}
+
+	private static void testFindByNameContainingOrderByAsc() {
+
+		List<Country> cList = countryService.findByNameContainingOrderByAsc("ou");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("COUTRIES CONTAINING ou IN NAME IN ASCENDING ORDER");
+		for (Country c : cList) {
+			System.out.println(c.getCode() + " " + c.getName());
+		}
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+	}
+
+	private static void testfindByNameStartingWith() {
+		List<Country> list = countryService.findByNameStartingWith("z");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
 		
-				ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
-				countryService = context.getBean(CountryService.class);
-				testGetAllCountries();
-				LOGGER.info("Inside main");
-		
+		System.out.println("COUNTRIES STARTING WITH Z");
+		for (Country c : list) {
+			System.out.println(c.getCode() + " " + c.getName());
+		}
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
 		
 	}
-	
-
 }

0bfcd5c094f5dc4011138cfa9ad99b53d9209af0
ADM21JF008009 <92528233+ADM21JF-008009@users.noreply.github.com>
2021-12-17 16:34:05 +0530

    Add files via upload

---
+++ Stage 3/Final Check/Movie/src/main/java/com/cognizant/moviecruiser/dao/FavoritesDaoCollectionImplTest.java
@@ -0,0 +1,16 @@
+package com.cognizant.moviecruiser.dao;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.cognizant.moviecruiser.model.Movie;
+
+public class FavoritesDaoCollectionImplTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String choice;
+		int movieAdded = 0;
+	}
+
+}
---
+++ Stage 3/Final Check/Movie/src/main/java/com/cognizant/moviecruiser/dao/MovieDaoCollectionImplTest.java
@@ -0,0 +1,17 @@
+package com.cognizant.moviecruiser.dao;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.cognizant.moviecruiser.model.Movie;
+import com.cognizant.moviecruiser.util.DateUtil;
+
+public class MovieDaoCollectionImplTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String choice;
+
+	}
+
+}
